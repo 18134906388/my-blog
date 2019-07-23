ba742b56042edd10a706dcfbde38ba707219c704
lizhiwei <0049003294@znv.com>
2019-07-23 10:55:31 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,6 +11,7 @@ module.exports = {
             { text: 'Home', link: '/' },
             { text: 'JavaScript中的一些问题', link: '/blog/JavaScript中的一些问题.md' }
         ],
+        sidebarDepth: 2,
         sidebar: [
             ['/', '首页'],
             ['/blog/JavaScript中的一些问题.md', 'JavaScript中的一些问题'],
@@ -20,6 +21,8 @@ module.exports = {
             ['/blog/项目开发中的坑.md', '项目开发中的坑'],
             ['/blog/江苏E项目总结.md', '江苏E项目总结'],
             ['/blog/面试遇到的问题.md', '面试遇到的问题'],
+            ['/blog/MySql问题总结.md', 'MySql问题总结'],
+            ['/blog/有趣的JS文章.md', '有趣的JS文章'],
             ['/blog/前端培训课件.md', '前端培训课件']
         ]
   }
---
+++ docs/blog/有趣的JS文章.md
@@ -0,0 +1,13 @@
+# 有趣的JS文章
+
+## JS常用的工具函数
+
+[JS常用的工具函数](https://mp.weixin.qq.com/s/aUZEWqcSiF7jI3Q0S2dRIQ)
+
+## JS技巧包含一些遍历函数的实现，函数化编程
+
+[一个合格的中级前端工程师要掌握的 JavaScript 技巧](https://mp.weixin.qq.com/s/X6ks8HrhAXvIjkcnZ9qMLw)
+
+## JS 中可以提升幸福度的小技巧
+
+[JS 中可以提升幸福度的小技巧](https://mp.weixin.qq.com/s/oOgdbH87ovn1lDLfiSPJcg)
