5b742d2df2bc59a1dc712429491585223d1adf96
lizhiwei <0049003294@znv.com>
2019-09-04 15:09:39 +0800

    update
    Signed-off-by: lizhiwei <0049003294@znv.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,7 +19,7 @@ module.exports = {
             ['/blog/VUE中的一些问题.md', 'VUE中的一些问题'],
             ['/blog/使用mpvue开发微信小程序遇到的坑.md', '使用mpvue开发微信小程序遇到的坑'],
             ['/blog/项目开发中的坑.md', '项目开发中的坑'],
-            ['/blog/江苏E项目总结.md', '江苏E项目总结'],
+            // ['/blog/江苏E项目总结.md', '江苏E项目总结'],
             ['/blog/面试遇到的问题.md', '面试遇到的问题'],
             ['/blog/MySql问题总结.md', 'MySql问题总结'],
             ['/blog/有趣的JS文章.md', '有趣的JS文章'],
