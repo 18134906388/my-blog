66f95232c6198c0d966e9840783a0cbc8edd3303
lizhiwei <0049003294@znv.com>
2021-04-08 15:20:36 +0800

    'up'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'my-blog',
     description: 'My blog by vuepress',
     head: [
-        ['link', { rel: 'shortcut icon', type: "image/png", href: `../vue-logo.png` }]
+        ['link', { rel: 'shortcut icon', type: "image/png", href: `/vue-logo.png` }]
     ],
     themeConfig: {
         // 你的GitHub仓库，请正确填写
@@ -18,23 +18,39 @@ module.exports = {
             apiKey: '45266fef189a0e1d66f56a13dbd73b07',
             indexName: 'rst-bj'
         },
-        sidebarDepth: 2,
+        sidebarDepth: 1,
         sidebar: [
             ['/', '首页'],
-            ['/blog/流媒体播放器.md', '流媒体播放器'],
-            ['/blog/gis总结.md', 'gis总结'],
             ['/blog/JS中遇到问题.md', 'JS中遇到问题'],
             ['/blog/CSS中遇到的问题.md', 'CSS中遇到的问题'],
             ['/blog/VUE中的一些问题.md', 'VUE中的一些问题'],
-            ['/blog/使用mpvue开发微信小程序遇到的坑.md', '使用mpvue开发微信小程序遇到的坑'],
-            ['/blog/项目开发中的坑.md', '项目开发中的坑'],
-            ['/blog/江苏E项目总结.md', '江苏E项目总结'],
-            ['/blog/上海R项目总结.md', '上海R项目总结'],
+            ['/blog/使用mpvue开发微信小程序遇到的坑.md', '小程序'],
             ['/blog/面试遇到的问题.md', '面试遇到的问题'],
             ['/blog/MySql问题总结.md', 'MySql问题总结'],
             ['/blog/云服务器配置.md', '云服务器配置'],
             ['/blog/有趣的JS文章.md', '有趣的JS文章'],
-            ['/blog/前端培训课件.md', '前端培训课件']
+            ['/blog/前端培训课件.md', '前端培训课件'],
+            {
+                title: '项目总结',   // 必要的
+                collapsable: true, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                    ['/blog/项目开发中的坑.md', '项目开发中的坑'],
+                    ['/blog/江苏E项目总结.md', '江苏E项目总结'],
+                    ['/blog/上海R项目总结.md', '上海R项目总结'],
+                    ['/blog/大数据可视化平台.md', '大数据可视化平台'],
+                    ['/blog/喜马拉雅项目总结.md', '喜马拉雅项目总结']
+                ]
+            },
+            {
+                title: '技术沉淀',   // 必要的
+                collapsable: true, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                    ['/blog/流媒体播放器.md', '流媒体播放器'],
+                    ['/blog/gis总结.md', 'gis总结']
+                ]
+            }
         ]
     },
     configureWebpack: {
--- docs/README.md
+++ docs/README.md
@@ -4,12 +4,28 @@ heroImage: /vue-logo.png
 heroText: My Blog
 tagline: 博客列表
 actionText: 快速上手 →
-actionLink: /blog/流媒体播放器.md
+actionLink: /blog/JS中遇到问题.md
 features:
-- title: 学习总结
-  details: 平时日常学习积累，包括前端三大栈的基础，以及拓展知识的记录。
-- title: 项目总结
-  details: 定期总结项目经历和经验，博观而约取,厚积而薄发。
-- title: 日常填坑
-  details: 及时记录平时遇到的坑，避免多次爬坑。
+  - title: 学习总结
+    details: 平时日常学习积累，包括前端三大栈的基础，以及拓展知识的记录。
+  - title: 项目总结
+    details: 定期总结项目经历和经验，博观而约取,厚积而薄发。
+  - title: 日常填坑
+    details: 及时记录平时遇到的坑，避免多次爬坑。
 ---
+```
+#nginx配置
+server {
+  listen       8000;
+  server_name  localhost;
+  location / {
+    root   /opt/web/lizhiwei_blog;
+    try_files $uri $uri/ /index.html;
+    index  index.html index.htm;
+  }
+  location ^~ /my-blog {
+    root   /opt/web/lizhiwei_blog;
+    rewrite  ^.+my-blog(.*)$ /$1 break;
+  }
+}
+```
